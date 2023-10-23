2506eac1f7eaa0161f5213f6bfdffed6823c19b9
Mangesh Kumar <mangesh@fountane>
2023-10-23 18:54:02 +0530

    fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { APP_NAME } from '@/utils/constants';
 import { useRef } from 'react';
-import Link from 'next/link';
 
 export default function Login() {
   const emailRef = useRef<HTMLInputElement>(null);
