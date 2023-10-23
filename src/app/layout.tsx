380caf6ebe3b7cee72b7062500ab4a646c2a71ec
Mangesh Kumar <mangesh@fountane>
2023-10-23 18:35:16 +0530

    fix: formatting changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { cn } from '@/lib/utils';
 import Navbar from '@/components/Navbar';
+import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en" className="light">
       <body
         className={cn(
-          'min-h-screen font-sans antialiased grainy',
+          'grainy min-h-screen font-sans antialiased',
           inter.className,
         )}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border cursor-pointer border-gray-200 bg-white px-8 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50 ">
+      <MaxWidthWrapper className="mb-12 mt-28 flex flex-col items-center justify-center text-center sm:mt-40">
+        <div className="mx-auto mb-4 flex max-w-fit cursor-pointer items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-8 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50 ">
           <p className="text-sm font-semibold text-gray-700">
             smartGPT is now public!!
           </p>
@@ -48,7 +48,7 @@ export default function Home() {
                 clipPath:
                   'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
               }}
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm-left-[calc(50%-30rem)] sm-:w-[72.1875]"
+              className="sm-left-[calc(50%-30rem)] sm-:w-[72.1875] relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
             />
           </div>
 
@@ -57,7 +57,7 @@ export default function Home() {
               <div className="mt-16 flow-root sm:mt-24">
                 <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                   <Image
-                    className="rounded-md bg-gray-500/20 p-2 sm:p-8 md:20 shadow-2xl ring-1 ring-gray-900/10"
+                    className="md:20 rounded-md bg-gray-500/20 p-2 shadow-2xl ring-1 ring-gray-900/10 sm:p-8"
                     src="/dashboard-preview.jpg"
                     width={3000}
                     height={2000}
@@ -87,7 +87,7 @@ export default function Home() {
       <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
         <div className="mb-12 px-6 lg:px-8">
           <div className="mx-auto max-w-2xl sm:text-center">
-            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+            <h2 className="mt-2 text-4xl font-bold text-gray-900 sm:text-5xl">
               Start chatting in minutes
             </h2>
             <p className="mt-4 text-lg text-gray-600">
@@ -151,7 +151,7 @@ export default function Home() {
                 width={1419}
                 height={732}
                 quality={100}
-                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                className="rounded-md bg-white p-2 shadow-2xl ring-1 ring-gray-900/10 sm:p-8 md:p-20"
               />
             </div>
           </div>
